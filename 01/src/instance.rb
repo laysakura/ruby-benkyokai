4ed722ec8165438c9bf9a4efeba9aa5348d96438
Sho Nakatani <lay.sakura@gmail.com>
2014-05-11 15:27:39 +0900

    OOPの導入前まで書いた

---
+++ 01/src/instance.rb
@@ -0,0 +1,9 @@
+class C
+  # インスタンスメソッド
+  def hello
+    p 'hello'
+  end
+end
+
+c = C.new  # クラスオブジェクトCからインスタンスオブジェクトcを生成
+c.hello
---
+++ 01/src/method.rb
@@ -0,0 +1,6 @@
+def print_anything(s)
+  p s
+end
+
+print_anything('hello')
+print_anything 'world'
