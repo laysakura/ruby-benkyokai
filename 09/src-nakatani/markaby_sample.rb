5d8f8c9a5e7b419d7bc014849396c32dff23ac27
Sho Nakatani <lay.sakura@gmail.com>
2014-07-07 06:59:56 +0900

    update

---
+++ 09/src-nakatani/markaby_sample.rb
@@ -0,0 +1,15 @@
+require 'markaby'
+
+mab = Markaby::Builder.new
+mab.html do
+  head { title "Boats.com" }
+  body do
+    h1 "Boats.com has great deals"
+    ul do
+      li "$49 for a canoe"
+      li "$39 for a raft"
+      li "$29 for a huge boot that floats and can fit 5 people"
+    end
+  end
+end
+puts mab.to_s
